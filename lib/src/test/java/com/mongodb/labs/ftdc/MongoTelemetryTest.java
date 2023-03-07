66a26dbd464afca667de5588d612fa603d1803f7
Jeff Yemin <jeff.yemin@mongodb.com>
2023-03-07 11:03:51 -0500

    Update test

--- lib/src/test/java/com/mongodb/labs/ftdc/MongoTelemetryTest.java
+++ lib/src/test/java/com/mongodb/labs/ftdc/MongoTelemetryTest.java
@@ -16,7 +16,7 @@ class MongoTelemetryTest {
         MongoTelemetry.addTelemetryListeners(clientSettingsBuilder);
         MongoClient client = MongoClients.create(clientSettingsBuilder.build());
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 30; i++) {
             client.getDatabase("admin").runCommand(new Document("ping", 1));
             Thread.sleep(100);
         }
